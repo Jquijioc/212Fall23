658937818ba1c4a2bcb5e0baf0af7de78986657a
Mike123456789123456789 <150295344+Mike123456789123456789@users.noreply.github.com>
2023-12-04 19:44:05 -0500

    Update quickSort.cpp
    Added comments

--- Sorting Algorithms/quickSort.cpp
+++ Sorting Algorithms/quickSort.cpp
@@ -2,46 +2,57 @@
 #include <vector>
 #include <fstream>
 
+// Function declarations
 void quicksort(std::vector<float>& arr, int low, int high, int& comparisons);
 int partition(std::vector<float>& arr, int low, int high, int& comparisons);
 
 int main(int argc, char *argv[]) {
+    // Check for proper usage and command line arguments
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
         return 1;
     }
 
+    // Reading the input file
     std::string filename = argv[1];
     std::vector<float> arr;
     std::ifstream file(filename);
 
+    // Check if file opening was successful
     if (!file) {
         std::cerr << "Unable to open file " << filename << std::endl;
         return 1;
     }
 
+    // Reading values from file into vector
     float value;
     while (file >> value) {
         arr.push_back(value);
     }
     file.close();
 
-    int comparisons = 0;  // Variable to store the number of comparisons
+    // Initialize a counter for the number of comparisons
+    int comparisons = 0;
+    // Perform quicksort on the array
     quicksort(arr, 0, arr.size() - 1, comparisons);
 
+    // Output the sorted array
     std::cout << "Sorted array: ";
     for (float i : arr) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
 
+    // Output the number of comparisons made during sorting
     std::cout << "Number of comparisons: " << comparisons << std::endl;
 
     return 0;
 }
 
+// Quicksort function
 void quicksort(std::vector<float>& arr, int low, int high, int& comparisons) {
     if (low < high) {
+        // Partition the array and get the pivot index
         int pi = partition(arr, low, high, comparisons);
 
         quicksort(arr, low, pi - 1, comparisons);
@@ -54,12 +65,12 @@ int partition(std::vector<float>& arr, int low, int high, int& comparisons) {
     int i = (low - 1);
 
     for (int j = low; j <= high - 1; j++) {
-        comparisons++;  // Increment comparisons for each comparison made
+        comparisons++;  // Increment comparison count
         if (arr[j] < pivot) {
-            i++;
+            i++;  // Increment index of smaller element
             std::swap(arr[i], arr[j]);
         }
     }
-    std::swap(arr[i + 1], arr[high]);
-    return (i + 1);
+    std::swap(arr[i + 1], arr[high]);  
+    return (i + 1);  
 }
